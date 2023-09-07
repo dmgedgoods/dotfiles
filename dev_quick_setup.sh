e0574d758bc2161d08bb5f62e351b565879871ad
dmgedgoods <119913388+dmgedgoods@users.noreply.github.com>
2023-09-07 14:52:18 -0700

    Update dev_quick_setup.sh

--- dev_quick_setup.sh
+++ dev_quick_setup.sh
@@ -9,6 +9,12 @@ sudo wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64
 sudo tar -zxvf /opt/nvim-linux64.tar.gz
 sudo ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
 
+sudo wget https://nodejs.org/dist/v18.17.1/node-v18.17.1-linux-x64.tar.xz
+sudo tar -xvf node-v18.17.1-linux-x64.tar.xz
+sudo ln -s /opt/node-v18.17.1-linux-x64/bin/node /usr/bin/node
+sudo ln -s /opt/node-v18.17.1-linux-x64/bin/npm /usr/bin/npm
+sudo ln -s /opt/node-v18.17.1-linux-x64/bin/npx /usr/bin/npx
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
@@ -18,12 +24,8 @@ cp -r dotfiles/nvim ~/.config
 cp -r dotfiles/tmux ~/.config
 rm -rf dotfiles
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
-source ~/.zshrc
 chsh -s $(which zsh)
 zsh
-
-nvm install 20
 
 echo "source packer.lua, :PackerSync, get to work!"

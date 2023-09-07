6f704aefdfa69af6dbf032e408772601ac227b51
dmgedgoods <119913388+dmgedgoods@users.noreply.github.com>
2023-09-07 14:35:35 -0700

    Update dev_quick_setup.sh

--- dev_quick_setup.sh
+++ dev_quick_setup.sh
@@ -16,12 +16,14 @@ cd
 git clone https://github.com/dmgedgoods/dotfiles.git
 cp -r dotfiles/nvim ~/.config
 cp -r dotfiles/tmux ~/.config
+rm -rf dotfiles
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+source ~/.zshrc
 chsh -s $(which zsh)
 zsh
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-source ~/.zshrc
+
 nvm install 20
 
 echo "source packer.lua, :PackerSync, get to work!"

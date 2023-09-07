0d2f86b75777367da038843db3020f0c880b36b5
dmgedgoods <119913388+dmgedgoods@users.noreply.github.com>
2023-09-07 14:26:41 -0700

    Update dev_quick_setup.sh

--- dev_quick_setup.sh
+++ dev_quick_setup.sh
@@ -4,8 +4,6 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt-get install -y curl zsh firefox git gh gcc 
 
-gh auth login
-
 cd /opt
 sudo wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo tar -zxvf /opt/nvim-linux64.tar.gz
@@ -14,10 +12,16 @@ sudo ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-git clone https://github.com/dmgegoods/dotfiles
+cd
+git clone https://github.com/dmgedgoods/dotfiles.git
 cp -r dotfiles/nvim ~/.config
 cp -r dotfiles/tmux ~/.config
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+chsh -s $(which zsh)
+zsh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+source ~/.zshrc
+nvm install 20
 
 echo "source packer.lua, :PackerSync, get to work!"

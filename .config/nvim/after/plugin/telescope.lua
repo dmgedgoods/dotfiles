13a1a0f0569bc9fda51df0f275e059619aff7a03
dmgedgoods <smcdani8@mail.ccsf.edu>
2023-09-21 18:44:09 -0700

    quick update

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -6,4 +6,19 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-
+vim.keymap.set("n", "<Leader>fhh", function()
+    require("telescope.builtin").grep_string(require("telescope.themes").get_dropdown({
+        prompt_title = "Help",
+        search = "",
+        search_dirs = vim.api.nvim_get_runtime_file("doc/*.txt", true),
+        only_sort_text = true,
+        disable_coordinates = true,
+        path_display = "hidden",
+        layout_config = {
+            anchor = "S",
+            prompt_position = "bottom",
+            height = 0.35,
+            width = 0.9,
+        },
+    }))
+end)

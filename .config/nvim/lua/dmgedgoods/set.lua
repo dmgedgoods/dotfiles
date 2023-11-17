b1f652486badd8084e625baddc050211f15b87d4
dmgedgoods <119913388+dmgedgoods@users.noreply.github.com>
2023-11-17 11:56:05 -0800

    Update set.lua

--- .config/nvim/lua/dmgedgoods/set.lua
+++ .config/nvim/lua/dmgedgoods/set.lua
@@ -13,6 +13,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
+-- Adjust for windows install. Will throw errors. 
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 

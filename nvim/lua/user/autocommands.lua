faa0fa67cc0379e987a15493558e47d807cc67f0
dmgedgoods <smcdani8@mail.ccsf.edu>
2024-02-07 16:40:56 -0800

    dotfiles update

---
+++ nvim/lua/user/autocommands.lua
@@ -0,0 +1,44 @@
+vim.cmd [[
+  augroup _general_settings
+    autocmd!
+    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200})
+    autocmd BufWinEnter * :set formatoptions-=cro
+    autocmd FileType qf set nobuflisted
+  augroup end
+
+  augroup VimTeX
+    autocmd!
+    autocmd BufReadPre /path/to/project/*.tex
+    \ let b:vimtex_main = '/path/to/project/main.tex'
+  augroup END
+
+  augroup _git
+    autocmd!
+    autocmd FileType gitcommit setlocal wrap
+    autocmd FileType gitcommit setlocal spell
+  augroup end
+
+  augroup _markdown
+    autocmd!
+    autocmd FileType markdown setlocal wrap
+    autocmd FileType markdown setlocal spell
+  augroup end
+
+  augroup _auto_resize
+    autocmd!
+    autocmd VimResized * tabdo wincmd =
+  augroup end
+
+  augroup _alpha
+    autocmd!
+    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+  augroup end
+
+   augroup _lsp
+    autocmd!
+    autocmd BufEnter * set formatoptions-=cro
+    autocmd BufEnter * setlocal formatoptions-=cro
+    autocmd BufWritePre * lua vim.lsp.buf.format()
+   augroup end
+]]

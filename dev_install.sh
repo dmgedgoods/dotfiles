33e25ac5fccd5b4df47608ce04c2b080698d3603
mack <mack@monke.group>
2025-02-23 14:02:21 -0800

    dev_install.sh

---
+++ dev_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install gh git curl wget gettext gcc make cmake
+gh auth login
+gh repo clone dmgedgoods/dotfiles
+mv dotfiles .dotfiles
+gh repo clone neovim/neovim
+cd neovim
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+sudo mv ~/neovim /opt
+cp -r ~/.dotfiles/.config/nvim ~/.config/
+cd ~/
+wget https://go.dev/dl/go1.24.0.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ~/go1.24.0.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+source ~/.bashrc

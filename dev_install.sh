01ea14aa3407cc4bee2ffd5a0fde1ae829df013f
mack <119913388+dmgedgoods@users.noreply.github.com>
2025-03-08 11:21:13 -0800

    Update dev_install.sh

--- dev_install.sh
+++ dev_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install gh git curl wget gettext gcc make cmake
+sudo apt install gh git curl wget gettext gcc make cmake zsh
 gh auth login
 gh repo clone dmgedgoods/dotfiles
 mv dotfiles .dotfiles
@@ -16,3 +16,4 @@ wget https://go.dev/dl/go1.24.0.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ~/go1.24.0.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 source ~/.bashrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

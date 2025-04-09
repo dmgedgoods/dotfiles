7796850d225726de0fe7a7fd0bc5e53388cd70b9
mack <mack@monke-mac.local>
2025-04-08 17:41:20 -0700

    nix inbound

--- .config/i3/display.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-xrandr --output eDP-1 --mode "1920x1080" --scale 1x1
-xrandr --output DP-1-0.1 --mode "1920x1080" --rate "60.00" --right-of HDMI-1-0 --rotate right --scale 1x1
-xrandr --output HDMI-1-0 --mode "2560x1440" --rate "60.00" --right-of eDP-1 --scale 1x1
-
-
